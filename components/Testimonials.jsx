754f4b0051b8f6e9aeb5fbd30071afd79fb34e06
Supamoto-it <44291482+DumisaniLesley@users.noreply.github.com>
2024-03-07 15:27:19 +0200

    Add testimonials data and update testimonials display

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -1,5 +1,33 @@
 import Image from 'next/image'
 import { FaStar } from "react-icons/fa";
+import { GoDash } from "react-icons/go";
+
+const testimonialsData = [
+  {
+    id: 1,
+    name: 'Kenny Zulu',
+    image: '/images/user.jpg',
+    emoji: '/emojis/care.png',
+    title: 'CEO of ABC Inc',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec turpis at nunc posuere tincidunt. Proin nec ex eget libero fermentum varius',
+  },
+  {
+    id: 2,
+    name: 'Morris Kangwa',
+    image: '/images/user.jpg',
+    emoji: '/emojis/care.png',
+    title: 'CEO of ABC Inc',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec turpis at nunc posuere tincidunt. Proin nec ex eget libero fermentum varius',
+  },
+  {
+    id: 3,
+    name: 'Alex Mwamba',
+    image: '/images/user.jpg',
+    emoji: '/emojis/care.png',
+    title: 'CEO of ABC Inc',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec turpis at nunc posuere tincidunt. Proin nec ex eget libero fermentum varius',
+  },
+]
 
 const Testimonials = () => {
   return (
@@ -7,28 +35,33 @@ const Testimonials = () => {
       <div>
         <h1 className='text-center'>Our Happy Customers</h1>
       </div>
-      <div>
-        <div className='w-[300px] mt-10 bg-white/10 backdrop-blur-xl py-2 px-4 rounded-xl'>
-          <div className='flex justify-center items-center gap-2'>
-            <Image src='/images/user.jpg' width={30} height={30} alt='image' className='rounded-full aspect-square'/>
-            <Image src='/emojis/care.png' width={30} height={30} alt='image' className='rounded-full aspect-square'/>
+      <div className='flex gap-4 '>
+        {testimonialsData.map((testimonial) => (
+          <div className='relative w-[250px] mt-10 bg-white/10 backdrop-blur-xl py-4 px-4 rounded-xl' key={testimonial.id}>
+            <div className='flex justify-center items-center px-2 py-1 rounded-full gap-2 bg-black absolute -top-4 right-[35%]'>
+              <Image src={testimonial.image} width={30} height={30} alt='image' className='rounded-full aspect-square'/>
+              <Image src={testimonial.emoji} width={30} height={30} alt='image' className='rounded-full aspect-square'/>
+            </div>
+            <div className='flex gap-1 my-2'>
+              <FaStar className='text-yellow-400 text-[10px]' />
+              <FaStar className='text-yellow-400 text-[10px]' />
+              <FaStar className='text-yellow-400 text-[10px]' />
+              <FaStar className='text-yellow-400 text-[10px]' />
+              <FaStar className='text-yellow-400 text-[10px]' />
+            </div>
+            <p className='text-[10px]'>{testimonial.desc}</p>
+            <div className='mt-4'>
+              <h4 className='text-[13px] font-bold'>{testimonial.name}</h4>
+              <p className='text-[8px]'>{testimonial.title}</p>
+            </div>
           </div>
-          <div className='flex gap-1 my-1'>
-            <FaStar className='text-yellow-400 text-sm' />
-            <FaStar className='text-yellow-400 text-sm' />
-            <FaStar className='text-yellow-400 text-sm' />
-            <FaStar className='text-yellow-400 text-sm' />
-            <FaStar className='text-yellow-400 text-sm' />
-          </div>
-          <p className='text-[10px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit.Inventore,dolor Inventore,dolor sit amet cons ectetur adipisicing elit. Inventore sed?</p>
-          <div className='mt-4'>
-            <h4>Name</h4>
-            <p className='text-sm'>Founder of Roblox</p>
-          </div>
-        </div>
+         ))}
       </div>
-      <div>
-        ----
+      <div className='flex justify-center mt-4'>
+        <GoDash className='text-5xl'/>
+        <GoDash className='text-5xl'/>
+        <GoDash className='text-5xl'/>
+        <GoDash className='text-5xl'/>
       </div>
     </div>
   )
