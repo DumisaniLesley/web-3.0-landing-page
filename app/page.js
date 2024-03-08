4c3cdb7b741ee2c468126587a9fc5c40289e7413
Supamoto-it <44291482+DumisaniLesley@users.noreply.github.com>
2024-03-08 19:34:05 +0200

    Add utility class to hide scrollbars

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="scroll-smooth">
       <Hero />
       <Services />
       <Numbers />
